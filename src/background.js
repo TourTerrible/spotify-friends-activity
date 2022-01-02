853bafdb15df761616b42a9caf42d907f8c8c025
ahad18abd <ahad.jnv22@gmail.com>
2022-01-03 01:06:32 +0530

    final commit

--- src/background.js
+++ src/background.js
@@ -50,25 +50,3 @@ chrome.runtime.onMessage.addListener( async(request,sender, sendResponse)=>{
     console.log("gettting activity");
 	}
 });
-
-
-//usage:
-// getCookies("https://open.spotify.com", "sp_dc", function(id) {
-//   console.log(id);
-// });
-
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   //main();
-//   if (request.type === 'GREETINGS') {
-//     const message = `Hi ${
-//       sender.tab ? 'Con' : 'Pop'
-//     }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-//     // Log message coming from the `request` parameter
-//     console.log(request.payload.message);
-//     // Send a response message
-//     sendResponse({
-//       message,
-//     });
-//   }
-// });
